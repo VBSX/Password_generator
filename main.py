9bb9a7bf21a82c38be811a7e3e07f79b337d0e25
Vitor <84788940+VBSX@users.noreply.github.com>
2022-08-16 21:22:41 -0300

    update dialog

--- main.py
+++ main.py
@@ -1,60 +1,66 @@
 import sys
 import random
-from turtle import width
-from PySide6 import QtCore, QtWidgets, QtGui
+from PySide2.QtWidgets import QPushButton, QMainWindow
+from PySide2 import QtCore
 
-class MyWidget(QtWidgets.QWidget):
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import QMessageBox, QMainWindow, QVBoxLayout,QApplication, QWidget, QLabel
+
+class MyWidget(QMainWindow):
     def __init__(self):
-        super().__init__()
-        self.numbers = ['0' ,'1', '2', '3', '4', '5', '6', '7', '8', '9']
+        super(MyWidget, self).__init__()
+        self.setWindowTitle('Password Generator')
         self.letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 
              'h', 'i', 'j', 'k', 'l', 'm', 'n',
              'o', 'p', 'q', 'r', 's', 't', 'u', 
              'v', 'w', 'x', 'y', 'z']
         self.especialchars = ['#', '@', '$', '%']
-        
-        self.setWindowTitle('Password Generator')
-        self.button = QtWidgets.QPushButton("Generate a password")
-        
-                                  
-        self.btn = QtWidgets.QPushButton('Copy to clipboard', clicked=self.copy_to_clipboard)
-        self.lbl = QtWidgets.QLabel(alignment=QtCore.Qt.AlignCenter)
-        self.text = QtWidgets.QLabel(alignment=QtCore.Qt.AlignCenter)
+        self.button = QPushButton("Generate a password")
+                          
+        self.btn = QPushButton('Copy to clipboard', clicked=self.copy_to_clipboard)
+        self.lbl = QLabel()
+        self.lbl.setAlignment(QtCore.Qt.AlignCenter)
+        self.text = QLabel()
+        self.text.setAlignment(QtCore.Qt.AlignCenter)
         self.text.setStyleSheet('color: rgb(128,0,0);')
 
-        self.layout = QtWidgets.QVBoxLayout(self)
+        widget = QWidget()
+        
+        self.layout = QVBoxLayout()
         self.layout.addWidget(self.text)
-        self.layout.addWidget(self.button)
-        self.layout.addWidget(self.btn)
-        self.layout.addWidget(self.lbl)
-        self.button.clicked.connect(self.magic)
+        # widget.setLayout(self.layout)
+        # self.setCentralWidget(widget)
+        # self.layout.addWidget(self.button)
+        # self.layout.addWidget(self.btn)
+        # self.layout.addWidget(self.lbl)
+        # self.button.clicked.connect(self.magic)
         
         
-    def copy_to_clipboard(self):
-        cb = QtWidgets.QApplication.clipboard()
+    def copy_to_clipboard(self): 
+        cb = QApplication.clipboard()
         cb.clear(mode=cb.Clipboard)
         cb.setText(self.text.text(),  mode=cb.Clipboard)
-        self.lbl.setText('Content was copied')
-        
+        self.show_dialog('Content was copied')
+              
         
     def magic(self):
-        self.text.setText(random.choice(self.numbers)+
-                          random.choice(self.numbers)+
-                          random.choice(self.numbers)+
-                          random.choice(self.numbers)+
-                          random.choice(self.numbers)+
+        self.text.setText(str(random.randrange(1, 10))+
+                          str(random.randrange(1, 10))+
+                          str(random.randrange(1, 10))+
+                          str(random.randrange(1, 10))+
+                          str(random.randrange(1, 10))+
                           random.choice(self.especialchars)+
                           random.choice(self.letters)+
                           random.choice(self.letters)+
                           random.choice(self.letters)+
                           random.choice(self.letters))
-        
+    
+    def show_dialog(self, text):
+        QMessageBox.about(self, text, "Message")
         
 if __name__ == "__main__":
-    app = QtWidgets.QApplication([])
-
+    app = QApplication([])
     widget = MyWidget()
     widget.resize(300, 200)
     widget.show()
-
-    sys.exit(app.exec())
+    sys.exit(app.exec_())
